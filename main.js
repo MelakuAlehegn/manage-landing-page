8e49014dfe09240bab946066bfdb52e7b966c4af
unknown <melakualehegn34@gmail.com>
2023-06-14 03:31:54 +0300

    mobile menu open/close functionality added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
+function showMenu() {
+  document.querySelector(".nav-links").classList.toggle('active');
+  document.querySelector(".humberger").classList.toggle('hide');
+  document.querySelector(".close").classList.toggle('show');
+}
